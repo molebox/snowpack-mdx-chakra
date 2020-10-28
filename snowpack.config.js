d920ebf8293cb93a4e1c0979edb437b1a2abaf27
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-10-28 13:58:00 +0100

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
